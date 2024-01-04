e9e7030bd35b8be2f360741194e67e7faa116db8
habvi <93956895+habvi@users.noreply.github.com>
2024-01-04 19:43:41 +0900

    add: Makegfile & base

---
+++ 00/ex01/includes/contact.hpp
@@ -0,0 +1,13 @@
+#ifndef __CONTACT_H__
+#define __CONTACT_H__
+
+class Contact {
+  public:
+	// constructor
+
+	// destructor
+
+  private:
+};
+
+#endif /*__CONTACT_H__*/
---
+++ 00/ex01/includes/phonebook.hpp
@@ -0,0 +1,13 @@
+#ifndef __PHONEBOOK_H__
+#define __PHONEBOOK_H__
+
+class Phonebook {
+  public:
+	// constructor
+
+	// destructor
+
+  private:
+};
+
+#endif /*__PHONEBOOK_H__*/
