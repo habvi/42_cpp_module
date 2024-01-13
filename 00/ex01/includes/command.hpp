322b28051d01f88931150c81641e14019c537cee
habvi <93956895+habvi@users.noreply.github.com>
2024-01-13 09:51:33 +0900

    update(cmd_add): return input EOF & FAILURE to exec_add(#3)

--- 00/ex01/includes/command.hpp
+++ 00/ex01/includes/command.hpp
@@ -10,12 +10,14 @@
 
 /* ADD */
 #define MSG_SAVE_SUCCESS "ADD Success!"
+#define MSG_SAVE_FAIL    "ADD Failed."
 
 /* SEARCH */
 #define SEARCH_INPUT_WIDTH 0
 #define MSG_SEARCH         "Enter the index of the contact to display details"
 #define MSG_RETRY_INPUT_INDEX                                                       \
 	"Error: Invalid index. Please enter the registerd contact index."
+#define MSG_EMPTY_CONTACT_INFO "Error: Empty contact information is not allowed."
 
 typedef enum e_command {
 	CMD_ADD,
@@ -23,6 +25,11 @@ typedef enum e_command {
 	CMD_EXIT,
 	CMD_INVALID,
 } t_command;
+
+typedef struct s_pair {
+	const std::string title;
+	std::string      &dst;
+} t_pair;
 
 void exec_add(Phonebook &phonebook);
 void exec_search(const Phonebook &phonebook);
--- 00/ex01/srcs/cmd_add.cpp
+++ 00/ex01/srcs/cmd_add.cpp
@@ -3,34 +3,67 @@
 #include "phonebook.hpp"
 #include "utils.hpp"
 #include <iostream>
+#include <string>
 
-// todo: not allow empty input
-static const t_contact_dto input_data() {
-	t_contact_dto    data;
-	static const int input_width = CONTACT_FIELD_MAX_WIDTH;
+// The field isn't allowed to be an empty string.
+static t_result input_each_data(const std::string &title, std::string &dst) {
+	const t_result result = input_left_align(title, dst, CONTACT_FIELD_MAX_WIDTH);
 
-	input_left_align(FIRST_NAME, data.first_name, input_width);
-	input_left_align(LAST_NAME, data.last_name, input_width);
-	input_left_align(NICK_NAME, data.nick_name, input_width);
-	input_left_align(PHONE_NUMBER, data.phone_number, input_width);
-	input_left_align(DARKEST_SECRET, data.darkest_secret, input_width);
-	return data;
+	if (result == EOF || result == FAILURE) {
+		return result;
+	}
+	if (dst.empty()) {
+		std::cerr << MSG_EMPTY_CONTACT_INFO << std::endl;
+		return FAILURE;
+	}
+	return (SUCCESS);
+}
+
+static t_result input_data(t_contact_dto &data) {
+	t_pair fields[] = {
+		{FIRST_NAME, data.first_name},
+		{LAST_NAME, data.last_name},
+		{NICK_NAME, data.nick_name},
+		{PHONE_NUMBER, data.phone_number},
+		{DARKEST_SECRET, data.darkest_secret}
+	};
+	const size_t size = sizeof(fields) / sizeof(fields[0]);
+
+	for (size_t i = 0; i < size; i++) {
+		t_result result = input_each_data(fields[i].title, fields[i].dst);
+		if (result == EOF || result == FAILURE) {
+			return result;
+		}
+	}
+	return SUCCESS;
 }
 
 static void convert_data_to_contact(Contact &contact, const t_contact_dto &data) {
 	contact = Contact(data);
 }
 
-static void input_contact_date(Contact &contact) {
-	const t_contact_dto data = input_data();
+static t_result input_contact_date(Contact &contact) {
+	t_contact_dto  data;
+	const t_result result = input_data(data);
 
+	if (result == EOF || result == FAILURE) {
+		return result;
+	}
 	convert_data_to_contact(contact, data);
+	return SUCCESS;
 }
 
 void exec_add(Phonebook &phonebook) {
-	Contact contact;
+	Contact        contact;
+	const t_result result = input_contact_date(contact);
 
-	input_contact_date(contact);
+	if (result == EOF) {
+		return;
+	}
+	if (result == FAILURE) {
+		std::cerr << MSG_SAVE_FAIL << std::endl;
+		return;
+	}
 	phonebook.save_contact(contact);
-	std::cout << MSG_SAVE_SUCCESS << std::endl;
+	std::cerr << MSG_SAVE_SUCCESS << std::endl;
 }
