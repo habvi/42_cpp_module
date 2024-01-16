0266fd39405b9313d1dfa46fbf5b31c4c805bf75
habvi <93956895+habvi@users.noreply.github.com>
2024-01-17 02:36:50 +0900

    add(zombieHorde): implement Zombie class

---
+++ 01/ex01/includes/Zombie.hpp
@@ -0,0 +1,26 @@
+#ifndef ZOMBIE_H
+#define ZOMBIE_H
+
+#include <string>
+
+#define COLOR_RED    "\033[31m"
+#define COLOR_PINK   "\033[35m"
+#define COLOR_END    "\033[0m"
+
+#define ZOMBIE_SOUND "BraiiiiiiinnnzzzZ..."
+#define ZOMBIE_DEAD  " is dead."
+
+class Zombie {
+  public:
+	Zombie();
+	explicit Zombie(const std::string &name);
+	~Zombie();
+
+  public:
+	void announce(void) const;
+
+  private:
+	std::string name_;
+};
+
+#endif /* ZOMBIE_H */
