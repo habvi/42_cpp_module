eec425a0807eb6d5721558013b05c588c6778eb5
habvi <93956895+habvi@users.noreply.github.com>
2024-02-03 04:01:54 +0900

    add(main): use File class, just create new file

--- 01/ex04/srcs/main.cpp
+++ 01/ex04/srcs/main.cpp
@@ -1,4 +1,5 @@
 #include "error.hpp"
+#include "file.hpp"
 #include <cstdlib>
 #include <iostream>
 #include <string>
@@ -7,16 +8,28 @@ static bool is_valid_argc(const int argc) {
 	return argc == 4;
 }
 
-int main(int argc, char *argv[]) {
+static bool create_replaced_file(
+	const std::string &filename, const std::string &s1, const std::string &s2
+) {
+	File file(filename, s1, s2);
+
+	return file.create_replaced_file();
+}
+
+int main(int argc, const char *argv[]) {
 	if (!is_valid_argc(argc)) {
 		put_error(ERR_INVALID_ARGS);
 		return EXIT_FAILURE;
 	}
 
-	std::string filename = argv[1];
-	std::string s1       = argv[2];
-	std::string s2       = argv[3];
+	// todo: handle error
+	const std::string filename = argv[1];
+	const std::string s1       = argv[2];
+	const std::string s2       = argv[3];
 	std::cout << filename << " " << s1 << " " << s2 << std::endl;
 
+	if (!create_replaced_file(filename, s1, s2)) {
+		return EXIT_FAILURE;
+	}
 	return EXIT_SUCCESS;
 }
