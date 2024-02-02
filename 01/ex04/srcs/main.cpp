66498f3b4bbbac95557fe1e03fab6895af41e34b
habvi <93956895+habvi@users.noreply.github.com>
2024-02-03 04:03:46 +0900

    add(main): handle arguments empty string

--- 01/ex04/srcs/main.cpp
+++ 01/ex04/srcs/main.cpp
@@ -8,6 +8,12 @@ static bool is_valid_argc(const int argc) {
 	return argc == 4;
 }
 
+static bool is_valid_string(
+	const std::string &filename, const std::string &s1, const std::string &s2
+) {
+	return !(filename.empty() || s1.empty() || s2.empty());
+}
+
 static bool create_replaced_file(
 	const std::string &filename, const std::string &s1, const std::string &s2
 ) {
@@ -22,11 +28,13 @@ int main(int argc, const char *argv[]) {
 		return EXIT_FAILURE;
 	}
 
-	// todo: handle error
 	const std::string filename = argv[1];
 	const std::string s1       = argv[2];
 	const std::string s2       = argv[3];
-	std::cout << filename << " " << s1 << " " << s2 << std::endl;
+	if (!is_valid_string(filename, s1, s2)) {
+		put_error(ERR_INVALID_ARGS);
+		return EXIT_FAILURE;
+	}
 
 	if (!create_replaced_file(filename, s1, s2)) {
 		return EXIT_FAILURE;
