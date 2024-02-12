219151c90e7591d066f5922b9dcea8fa8b67f4a7
habvi <93956895+habvi@users.noreply.github.com>
2024-02-13 04:15:23 +0900

    update(main): allow empty s2

--- 01/ex04/srcs/main.cpp
+++ 01/ex04/srcs/main.cpp
@@ -13,10 +13,8 @@ static void put_guide() {
 	std::cerr << message << std::endl;
 }
 
-static bool is_valid_string(
-	const std::string &filename, const std::string &s1, const std::string &s2
-) {
-	return !(filename.empty() || s1.empty() || s2.empty());
+static bool is_valid_string(const std::string &filename, const std::string &s1) {
+	return !(filename.empty() || s1.empty());
 }
 
 static bool create_replaced_file(
@@ -37,7 +35,7 @@ int main(int argc, const char *argv[]) {
 	const std::string filename = argv[1];
 	const std::string s1       = argv[2];
 	const std::string s2       = argv[3];
-	if (!is_valid_string(filename, s1, s2)) {
+	if (!is_valid_string(filename, s1)) {
 		put_error(ERR_INVALID_ARGS);
 		return EXIT_FAILURE;
 	}
