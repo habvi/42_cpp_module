daf954a3287fe7c24acd69fefa3cc57317a18cd2
habvi <93956895+habvi@users.noreply.github.com>
2024-02-15 00:43:27 +0900

    add(Fixed): implement Fixed class

---
+++ 02/ex00/includes/Fixed.hpp
@@ -0,0 +1,21 @@
+#ifndef FIXED_H
+#define FIXED_H
+
+class Fixed {
+  public:
+	Fixed();
+	Fixed(const Fixed &f);
+	const Fixed &operator=(const Fixed &f);
+	~Fixed();
+
+  public:
+	// Following the instructions, not camelcase.
+	int  getRawBits(void) const;
+	void setRawBits(int const raw);
+
+  private:
+	int              fixed_point_number_;
+	static const int kNumOfFractionalBits_ = 8;
+};
+
+#endif /* FIXED_H */
