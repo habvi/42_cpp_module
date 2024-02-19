a0c51106fa93e8a618c166457f72fe41b01ed9be
habvi <93956895+habvi@users.noreply.github.com>
2024-02-20 05:27:09 +0900

    add(main): ex01 test (#19)

--- 02/ex01/srcs/main.cpp
+++ 02/ex01/srcs/main.cpp
@@ -1,15 +1,15 @@
 #include "Fixed.hpp"
-#include <iostream>
-
-/*
+#include <climits>
 #include <iomanip>
+#include <iostream>
 
 static void RunInt(const int number) {
 	std::cout << "-------------------------" << std::endl;
 	const Fixed f(number);
 
-	std::cout << std::fixed << std::setprecision(10);
+	std::cout << std::fixed << std::setprecision(8);
 	std::cout << "number : " << number << std::endl;
+	std::cout << "rawbit : " << f.getRawBits() << std::endl;
 	std::cout << "float  : " << f << std::endl;
 	std::cout << "int    : " << f.toInt() << std::endl;
 }
@@ -18,33 +18,52 @@ static void RunFloat(const float number) {
 	std::cout << "-------------------------" << std::endl;
 	const Fixed f(number);
 
-	std::cout << std::fixed << std::setprecision(10);
+	std::cout << std::fixed << std::setprecision(8);
 	std::cout << "number : " << number << std::endl;
+	std::cout << "rawbit : " << f.getRawBits() << std::endl;
 	std::cout << "float  : " << f << std::endl;
 	std::cout << "int    : " << f.toInt() << std::endl;
 }
 
 static void RunOriginalTest() {
 	std::cout << "\n----- original test -----" << std::endl;
+	static const int   kNumOfFractionalBits  = 8;
+	static const float MinRepresentableFloat = 1.f / (1 << kNumOfFractionalBits);
+
+	// int
 	RunInt(0);
 	RunInt(12345);
-	RunInt(2147483647);
-	RunInt(-2147483648);
+	RunInt(INT_MAX);
+	RunInt(INT_MIN);
 	RunInt(-12345);
-	RunInt(2147483647 >> 8);
-	RunInt((2147483647 >> 8) + 1);
-	RunInt(-2147483648 >> 8);
-	RunInt((-2147483648 >> 8) - 1);
+	RunInt(INT_MAX >> kNumOfFractionalBits);
+	RunInt((INT_MAX >> kNumOfFractionalBits) + 1);
+	RunInt(INT_MIN >> kNumOfFractionalBits);
+	RunInt((INT_MIN >> kNumOfFractionalBits) - 1);
 
+	// float
 	RunFloat(0.0f);
 	RunFloat(0.123f);
 	RunFloat(123.0f);
 	RunFloat(1.23456789f);
 	RunFloat(1234567.89f);
 	RunFloat(123456789.0f);
+	RunFloat(-0.123456789f);
 	RunFloat(0.123456789f);
+
+	// 0000 0000 0000 0000 0000 0000 0111 1011.1111 1111
+	RunFloat(123.99609375f);
+	// 0000 0000 0000 0000 0000 0000 0111 1011.1111 1110   (123.9921875)
+	RunFloat(123.99609375f - MinRepresentableFloat);
+	// 0000 0000 0000 0000 0000 0000 0111 1011.1111 1110 1 (123.994140625)
+	RunFloat(123.99609375f - MinRepresentableFloat / 2);
+	// 0000 0000 0111 1111 1111 1111 1111 1111.1111 1111
+	RunFloat(8388607.99609375f);
+	// 0000 0000 0000 0000 1111 1111 1111 1111.1111 1111
+	RunFloat(65535.99609375f);
+	// 0000 0000 0000 0001 0000 0000 0000 0000.0000 0000
+	RunFloat(65535.99609375f + MinRepresentableFloat);
 }
-*/
 
 int main() {
 	Fixed       a;
@@ -64,7 +83,7 @@ int main() {
 	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
 	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
 
-	// RunOriginalTest();
+	RunOriginalTest();
 
 	return 0;
 }
