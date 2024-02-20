7ef9070f11dfc53f26bf449d1c2efe3360e7f49d
habvi <93956895+habvi@users.noreply.github.com>
2024-02-20 23:49:53 +0900

    update(main): test1

--- 03/ex00/srcs/main.cpp
+++ 03/ex00/srcs/main.cpp
@@ -6,6 +6,15 @@
 #define COLOR_RED "\033[31m"
 #define COLOR_END "\033[0m"
 
+static void DisplayTitle(const std::string &title) {
+	std::cout << "\n┃ test: " << title << std::endl;
+	std::cout << "┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" << std::endl;
+}
+
+static void Line() {
+	std::cout << "----------------------" << std::endl;
+}
+
 static void PutStatusAandB(const ClapTrap &attacker, const ClapTrap &defender) {
 	attacker.PutStatus();
 	defender.PutStatus();
@@ -31,26 +40,33 @@ static void AttackAtoB(
 	}
 }
 
-static void RunOriginalTest() {
+static void RunTest1() {
+	DisplayTitle("No Hit Points");
+
 	ClapTrap    alice("Alice");
 	std::string target = "Bob";
 	ClapTrap    bob(target);
 
 	PutStatusAandB(alice, bob);
-	// Alice attacks Bob normal
+
+	// Alice -> Bob. Success.
 	AttackAtoB(alice, bob, target, 4);
 	PutStatusAandB(alice, bob);
 	// damages >= hit_points
 	AttackAtoB(alice, bob, target, 8);
 	PutStatusAandB(alice, bob);
-	// 0 damages
+	// 0 damages. Nothing happend.
 	AttackAtoB(alice, bob, target, 0);
 	PutStatusAandB(alice, bob);
 
-	// Bob -> Alice. Bob has no hit_points
+	// Bob -> Alice. Bob has no hit_points. Nothing happend.
 	target = "Alice";
 	AttackAtoB(bob, alice, target, 5);
 	PutStatusAandB(alice, bob);
+}
+
+static void RunOriginalTest() {
+	RunTest1();
 }
 
 int main() {
