51dd7586bfdf8c5cafaa24e2ab981e6b32bc3d0b
habvi <93956895+habvi@users.noreply.github.com>
2024-02-20 21:42:29 +0900

    add(main): attack A to B base

--- 03/ex00/srcs/main.cpp
+++ 03/ex00/srcs/main.cpp
@@ -1,10 +1,39 @@
 #include "ClapTrap.hpp"
 #include <cstdlib>
+#include <iostream>
 #include <string>
 
-static void PutStatusAandB(const ClapTrap &a, const ClapTrap &b) {
-	a.PutStatus();
-	b.PutStatus();
+#define COLOR_RED "\033[31m"
+#define COLOR_END "\033[0m"
+
+static void PutStatusAandB(const ClapTrap &attacker, const ClapTrap &defender) {
+	attacker.PutStatus();
+	defender.PutStatus();
+}
+
+static bool IsEnergyPointsLeft(const ClapTrap &c) {
+	return c.GetEnergyPoints() > 0;
+}
+
+static bool
+AreBothHitPointsLeft(const ClapTrap &attacker, const ClapTrap &defender) {
+	return (attacker.GetHitPoints() > 0) && (defender.GetHitPoints() > 0);
+}
+
+static void AttackAtoB(
+	ClapTrap          &attacker,
+	ClapTrap          &defender,
+	const std::string &target,
+	const unsigned int damages
+) {
+	if (IsEnergyPointsLeft(attacker) && AreBothHitPointsLeft(attacker, defender)) {
+		// attacker.SetAttackDamage(damages);
+		attacker.attack(target);
+		// defender.takeDamage(damages);
+		// attacker.LostEnergyPoint();
+	} else {
+		std::cout << COLOR_RED "Attack failed." COLOR_END << std::endl;
+	}
 }
 
 static void RunOriginalTest() {
@@ -12,6 +41,9 @@ static void RunOriginalTest() {
 	const std::string target = "Bob";
 	ClapTrap          bob(target);
 
+	PutStatusAandB(alice, bob);
+	// Alice attacks Bob normal
+	AttackAtoB(alice, bob, target, 4);
 	PutStatusAandB(alice, bob);
 }
 
