391dd3449df547e0c6156db0f087c38362749a7c
habvi <93956895+habvi@users.noreply.github.com>
2024-02-20 21:04:08 +0900

    add(ClapTrap): implement ClapTrap class

---
+++ 03/ex00/includes/ClapTrap.hpp
@@ -0,0 +1,35 @@
+#ifndef CLAP_TRAP_H
+#define CLAP_TRAP_H
+
+#include <string>
+
+class ClapTrap {
+  public:
+	ClapTrap(const std::string &name);
+	ClapTrap(const ClapTrap &c);
+	~ClapTrap();
+	const ClapTrap &operator=(const ClapTrap &c);
+
+  public:
+	// Following the instructions, not camelcase.
+	void attack(const std::string &target);
+	void takeDamage(unsigned int amount);
+	void btRepaired(unsigned int amount);
+
+  public:
+	const std::string &GetName() const;
+	unsigned int       GetHitPoints() const;
+	unsigned int       GetEnergyPoints() const;
+	unsigned int       GetAttackDamage() const;
+
+  private:
+	ClapTrap();
+
+  private:
+	std::string  name_;
+	unsigned int hit_points_;
+	unsigned int energy_points_;
+	unsigned int attack_damage_;
+};
+
+#endif /* CLAP_TRAP_H */
---
+++ 03/ex00/srcs/ClapTrap.cpp
@@ -0,0 +1,63 @@
+#include "ClapTrap.hpp"
+#include <iostream>
+
+ClapTrap::ClapTrap() {
+	std::cout << "Default constructor called" << std::endl;
+}
+
+ClapTrap::ClapTrap(const std::string &name)
+	: name_(name), hit_points_(10), energy_points_(10), attack_damage_(0) {
+	std::cout << "Constructor called" << std::endl;
+}
+
+// copy constructor
+ClapTrap::ClapTrap(const ClapTrap &c)
+	: name_(c.GetName()),
+	  hit_points_(c.GetHitPoints()),
+	  energy_points_(c.GetEnergyPoints()),
+	  attack_damage_(c.GetAttackDamage()) {
+	std::cout << "Copy constructor called" << std::endl;
+}
+
+ClapTrap::~ClapTrap() {
+	std::cout << "Destructor called" << std::endl;
+}
+
+// copy assignment operator overload
+const ClapTrap &ClapTrap::operator=(const ClapTrap &c) {
+	if (this != &c) {
+		name_          = c.GetName();
+		hit_points_    = c.GetHitPoints();
+		energy_points_ = c.GetEnergyPoints();
+		attack_damage_ = c.GetAttackDamage();
+	}
+	return *this;
+}
+
+void ClapTrap::attack(const std::string &target) {
+	(void)target;
+}
+
+void ClapTrap::takeDamage(unsigned int amount) {
+	(void)amount;
+}
+
+void ClapTrap::btRepaired(unsigned int amount) {
+	(void)amount;
+}
+
+const std::string &ClapTrap::GetName() const {
+	return name_;
+}
+
+unsigned int ClapTrap::GetHitPoints() const {
+	return hit_points_;
+}
+
+unsigned int ClapTrap::GetEnergyPoints() const {
+	return energy_points_;
+}
+
+unsigned int ClapTrap::GetAttackDamage() const {
+	return attack_damage_;
+}
