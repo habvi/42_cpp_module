12510332b0d5eba23a6cfb1a2a597b4bbbfa0e10
habvi <93956895+habvi@users.noreply.github.com>
2024-02-22 03:31:05 +0900

    add(ex02): ClapTrap & ScavTrap class from ex01 & Makefile

---
+++ 03/ex02/includes/ScavTrap.hpp
@@ -0,0 +1,21 @@
+#ifndef SCAV_TRAP_H
+#define SCAV_TRAP_H
+
+#include "ClapTrap.hpp"
+
+class ScavTrap : public ClapTrap {
+  public:
+	explicit ScavTrap(const std::string &name);
+	~ScavTrap();
+
+  public:
+	// Following the instructions, not camelcase.
+	void guardGate() const;
+
+  private:
+	static const unsigned int kInitialHitPoints    = 100;
+	static const unsigned int kInitialEnergyPoints = 50;
+	static const unsigned int kInitialAttackDamage = 20;
+};
+
+#endif /* SCAV_TRAP_H */
