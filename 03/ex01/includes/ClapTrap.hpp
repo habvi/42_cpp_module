cbd2c28d1f64341e970ee1a068dc907c4f9c9187
habvi <93956895+habvi@users.noreply.github.com>
2024-02-22 00:53:41 +0900

    add(ClapTrap): class ClapTrap from ex00 & Makefile

---
+++ 03/ex01/includes/ClapTrap.hpp
@@ -0,0 +1,44 @@
+#ifndef CLAP_TRAP_H
+#define CLAP_TRAP_H
+
+#include <string>
+
+class ClapTrap {
+  public:
+	explicit ClapTrap(const std::string &name);
+	ClapTrap(const ClapTrap &c);
+	~ClapTrap();
+	const ClapTrap &operator=(const ClapTrap &c);
+
+  public:
+	// Following the instructions, not camelcase.
+	void attack(const std::string &target);
+	void takeDamage(unsigned int amount);
+	void beRepaired(unsigned int amount);
+
+  public:
+	void AttackToDefender(
+		ClapTrap &defender, const std::string &target, const unsigned int damages
+	);
+	void PutStatus() const;
+	bool IsActionPossible() const;
+	void LoseEnergyPoint();
+
+  public:
+	const std::string &GetName() const;
+	unsigned int       GetHitPoints() const;
+	unsigned int       GetEnergyPoints() const;
+	unsigned int       GetAttackDamage() const;
+	void               SetAttackDamage(const unsigned int damages);
+
+  private:
+	ClapTrap();
+
+  private:
+	std::string  name_;
+	unsigned int hit_points_;
+	unsigned int energy_points_;
+	unsigned int attack_damage_;
+};
+
+#endif /* CLAP_TRAP_H */
