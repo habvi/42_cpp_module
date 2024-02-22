0db41c08b2ebeb0a59a5acb1661b61f2b754c973
habvi <93956895+habvi@users.noreply.github.com>
2024-02-22 20:10:00 +0900

    add(ClapTrap,ScavTrap,FragTrap): copy ex02 files

---
+++ 03/ex03/includes/FragTrap.hpp
@@ -0,0 +1,26 @@
+#ifndef FLAG_TRAP_H
+#define FLAG_TRAP_H
+
+#include "ClapTrap.hpp"
+
+class FragTrap : public ClapTrap {
+  public:
+	explicit FragTrap(const std::string &name);
+	FragTrap(const FragTrap &f);
+	~FragTrap();
+	const FragTrap &operator=(const FragTrap &f);
+
+  public:
+	// Following the instructions, not camelcase and const qualifier isn't used.
+	void highFivesGuys(void);
+
+  private:
+	FragTrap();
+
+  private:
+	static const unsigned int kInitialHitPoints    = 100;
+	static const unsigned int kInitialEnergyPoints = 100;
+	static const unsigned int kInitialAttackDamage = 30;
+};
+
+#endif /* FLAG_TRAP_H */
