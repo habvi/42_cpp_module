bfa356a648ecd9b307b5512098b6b77c4792ceec
habvi <93956895+habvi@users.noreply.github.com>
2024-02-23 01:34:38 +0900

    add(DiamondTrap): implement DiamondTrap class inhelit ScavTrap & FragTrap

---
+++ 03/ex03/includes/DiamondTrap.hpp
@@ -0,0 +1,27 @@
+#ifndef DIAMOND_TRAP_H
+#define DIAMOND_TRAP_H
+
+#include "FragTrap.hpp"
+#include "ScavTrap.hpp"
+
+static const std::string nama_surfix = "_clap_name";
+
+class DiamondTrap : public ScavTrap, public FragTrap {
+  public:
+	explicit DiamondTrap(const std::string &name);
+	DiamondTrap(const DiamondTrap &d);
+	~DiamondTrap();
+	const DiamondTrap &operator=(const DiamondTrap &d);
+
+  public:
+	const std::string &GetName() const;
+
+  private:
+	DiamondTrap();
+
+  private:
+	// same variable’s name than the one in the ClapTrap base class.
+	std::string name_;
+};
+
+#endif /* DIAMOND_TRAP_H */
---
+++ 03/ex03/srcs/DiamondTrap.cpp
@@ -0,0 +1,44 @@
+#include "DiamondTrap.hpp"
+#include <iostream>
+#include <string>
+
+#define COLOR_PINK "\033[35m"
+#define COLOR_END  "\033[0m"
+
+DiamondTrap::DiamondTrap() : ClapTrap() {}
+
+DiamondTrap::DiamondTrap(const std::string &name)
+	: ClapTrap(name + nama_surfix), name_(name) {
+	std::cout << "Constructor called (DiamondTrap)" << std::endl;
+	SetHitPoints(FragTrap::kInitialHitPoints);
+	SetEnergyPoints(ScavTrap::kInitialEnergyPoints);
+	SetAttackDamage(FragTrap::kInitialAttackDamage);
+}
+
+// copy constructor
+DiamondTrap::DiamondTrap(const DiamondTrap &d)
+	: ClapTrap(d), ScavTrap(d), FragTrap(d), name_(d.GetName()) {
+	std::cout << "Copy constructor called (DiamondTrap)" << std::endl;
+	SetHitPoints(d.GetHitPoints());
+	SetEnergyPoints(d.GetEnergyPoints());
+	SetAttackDamage(d.GetAttackDamage());
+}
+
+DiamondTrap::~DiamondTrap() {
+	std::cout << "Destructor called (DiamondTrap)" << std::endl;
+}
+
+// copy assignment operator overload
+const DiamondTrap &DiamondTrap::operator=(const DiamondTrap &d) {
+	if (this != &d) {
+		ClapTrap::operator=(d);
+		SetHitPoints(d.GetHitPoints());
+		SetEnergyPoints(d.GetEnergyPoints());
+		SetAttackDamage(d.GetAttackDamage());
+	}
+	return *this;
+}
+
+const std::string &DiamondTrap::GetName() const {
+	return name_;
+}
