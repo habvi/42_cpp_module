64ca2ae6f73c5a7fb75788d3e2be05ea77280c6d
habvi <93956895+habvi@users.noreply.github.com>
2024-02-22 20:03:37 +0900

    update(main/ex00): If the test fails, exit with code 1. Add a test to compare the expected result. (#24)

--- 03/ex00/srcs/main.cpp
+++ 03/ex00/srcs/main.cpp
@@ -4,16 +4,39 @@
 #include <iostream>
 #include <string>
 
-#define COLOR_RED "\033[31m"
-#define COLOR_END "\033[0m"
+#define COLOR_RED   "\033[31m"
+#define COLOR_GREEN "\033[32m"
+#define COLOR_END   "\033[0m"
+
+#define ALICE       "Alice"
+#define BOB         "Bob"
 
 static void DisplayTitle(const std::string &title) {
-	std::cout << "\n┃ test: " << title << std::endl;
+	std::cout << "\n\n┃ test: " << title << std::endl;
 	std::cout << "┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" << std::endl;
 }
 
 static void Line() {
 	std::cout << "----------------------" << std::endl;
+}
+
+template <typename T>
+static void JudgeIsEqual(
+	const std::string &message,
+	T                 &target,
+	const unsigned int expected_hit_points,
+	const unsigned int expected_energy_points,
+	const unsigned int expected_attack_damage
+) {
+	std::cout << message << ": ";
+	if (target.GetHitPoints() == expected_hit_points &&
+		target.GetEnergyPoints() == expected_energy_points &&
+		target.GetAttackDamage() == expected_attack_damage) {
+		std::cout << COLOR_GREEN "OK" << COLOR_END << std::endl;
+	} else {
+		std::cout << COLOR_RED "NG" << COLOR_END << std::endl;
+		exit(EXIT_FAILURE);
+	}
 }
 
 static void PutStatusAandB(const ClapTrap &attacker, const ClapTrap &defender) {
@@ -56,107 +79,127 @@ static void Repaire(ClapTrap &c, const unsigned int points) {
 static void RunTest1() {
 	DisplayTitle("No Hit Points");
 
-	ClapTrap    alice("Alice");
-	std::string target = "Bob";
-	ClapTrap    bob(target);
+	ClapTrap    alice(ALICE);
+	ClapTrap    bob(BOB);
+	std::string target = BOB;
 
 	PutStatusAandB(alice, bob);
+	JudgeIsEqual(" - Alice Init", alice, 10, 10, 0);
+	JudgeIsEqual(" - Bob Init  ", bob, 10, 10, 0);
 
 	// Alice -> Bob. Success.
 	AttackAtoB(alice, bob, target, 4);
-	PutStatusAandB(alice, bob);
+	JudgeIsEqual(" - Alice", alice, 10, 9, 4);
+	JudgeIsEqual(" - Bob  ", bob, 6, 10, 0);
 	// damages >= hit_points
 	AttackAtoB(alice, bob, target, 8);
-	PutStatusAandB(alice, bob);
+	JudgeIsEqual(" - Alice", alice, 10, 8, 8);
+	JudgeIsEqual(" - Bob  ", bob, 0, 10, 0);
 	// 0 damages. Nothing happend.
 	AttackAtoB(alice, bob, target, 0);
-	PutStatusAandB(alice, bob);
+	JudgeIsEqual(" - Alice", alice, 10, 8, 8);
+	JudgeIsEqual(" - Bob  ", bob, 0, 10, 0);
 
 	// Bob -> Alice. Bob has no hit_points. Nothing happend.
-	target = "Alice";
+	target = ALICE;
 	AttackAtoB(bob, alice, target, 5);
-	PutStatusAandB(alice, bob);
+	JudgeIsEqual(" - Alice", alice, 10, 8, 8);
+	JudgeIsEqual(" - Bob  ", bob, 0, 10, 0);
 }
 
 static void RunTest2() {
 	DisplayTitle("No Energy points for attack");
 
-	ClapTrap    alice("Alice");
-	std::string target = "Bob";
-	ClapTrap    bob(target);
+	ClapTrap          alice(ALICE);
+	ClapTrap          bob(BOB);
+	const std::string target = BOB;
 
 	bob.beRepaired(10);
 	PutStatusAandB(alice, bob);
+	JudgeIsEqual(" - Alice Init", alice, 10, 10, 0);
+	JudgeIsEqual(" - Bob Init  ", bob, 20, 9, 0);
 
 	// Alice -> Bob. 10 attacks.
 	for (unsigned int i = 0; i < 10; i++) {
 		AttackAtoB(alice, bob, target, 1);
+		JudgeIsEqual(" - Alice", alice, 10, 10 - i - 1, 1);
+		JudgeIsEqual(" - Bob  ", bob, 20 - i - 1, 9, 0);
 		Line();
 	}
-	PutStatusAandB(alice, bob);
 
 	// Alice -> Bob. Alice has no energy points. Nothing happend.
 	AttackAtoB(alice, bob, target, 1);
-	PutStatusAandB(alice, bob);
+	JudgeIsEqual(" - Alice", alice, 10, 0, 1);
+	JudgeIsEqual(" - Bob  ", bob, 10, 9, 0);
 }
 
 static void RunTest3() {
 	DisplayTitle("No Energy points for Repaire");
 
-	ClapTrap alice("Alice");
+	ClapTrap alice(ALICE);
 	alice.PutStatus();
+	JudgeIsEqual(" - Alice Init", alice, 10, 10, 0);
 
 	// Alice repaires 10 times.
 	for (unsigned int i = 0; i < 10; i++) {
 		Repaire(alice, 1);
+		JudgeIsEqual(" - Alice", alice, 10 + i + 1, 10 - i - 1, 0);
 		Line();
 	}
-	alice.PutStatus();
 
 	// Alice has no energy points. Nothing happend.
 	Repaire(alice, 1);
-	alice.PutStatus();
+	JudgeIsEqual(" - Alice", alice, 20, 0, 0);
 }
 
 static void RunTest4() {
 	DisplayTitle("Repaire overflow");
 
-	ClapTrap alice("Alice");
+	ClapTrap alice(ALICE);
 	alice.beRepaired(UINT_MAX - 20);
 	alice.PutStatus();
+	JudgeIsEqual(" - Alice Init", alice, UINT_MAX - 10, 9, 0);
 
 	// Max Hit points.
 	Repaire(alice, 11);
-	alice.PutStatus();
+	JudgeIsEqual(" - Alice", alice, UINT_MAX, 8, 0);
 
 	Repaire(alice, 0);
-	alice.PutStatus();
+	JudgeIsEqual(" - Alice", alice, UINT_MAX, 7, 0);
 }
 
 // same behavior test1
 static void RunTest5() {
 	DisplayTitle("Attack A to B in ClapTrap class");
 
-	ClapTrap    alice("Alice");
-	std::string target = "Bob";
-	ClapTrap    bob(target);
+	ClapTrap    alice(ALICE);
+	ClapTrap    bob(BOB);
+	std::string target = BOB;
 
 	PutStatusAandB(alice, bob);
+	JudgeIsEqual(" - Alice Init", alice, 10, 10, 0);
+	JudgeIsEqual(" - Bob Init  ", bob, 10, 10, 0);
 
 	// Alice -> Bob. Success.
 	alice.AttackToDefender(bob, target, 4);
-	PutStatusAandB(alice, bob);
+	JudgeIsEqual(" - Alice", alice, 10, 9, 4);
+	JudgeIsEqual(" - Bob  ", bob, 6, 10, 0);
+
 	// damages >= hit_points
 	alice.AttackToDefender(bob, target, 8);
-	PutStatusAandB(alice, bob);
+	JudgeIsEqual(" - Alice", alice, 10, 8, 8);
+	JudgeIsEqual(" - Bob  ", bob, 0, 10, 0);
+
 	// 0 damages. Nothing happend.
 	alice.AttackToDefender(bob, target, 0);
-	PutStatusAandB(alice, bob);
+	JudgeIsEqual(" - Alice", alice, 10, 8, 8);
+	JudgeIsEqual(" - Bob  ", bob, 0, 10, 0);
 
 	// Bob -> Alice. Bob has no hit_points. Nothing happend.
-	target = "Alice";
+	target = ALICE;
 	bob.AttackToDefender(alice, target, 5);
-	PutStatusAandB(alice, bob);
+	JudgeIsEqual(" - Alice", alice, 10, 8, 8);
+	JudgeIsEqual(" - Bob  ", bob, 0, 10, 0);
 }
 
 static void RunOriginalTest() {
