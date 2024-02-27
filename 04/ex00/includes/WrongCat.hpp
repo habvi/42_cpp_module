4e7438b26b6863714910001478ed92e53a1be566
habvi <93956895+habvi@users.noreply.github.com>
2024-02-28 01:48:42 +0900

    add(WrongAnimal,WrongCat): implement Wrong 2 classes

---
+++ 04/ex00/includes/WrongCat.hpp
@@ -0,0 +1,20 @@
+#ifndef EX00_WRONG_CAT_HPP
+#define EX00_WRONG_CAT_HPP
+
+#include "WrongAnimal.hpp"
+#include <string>
+
+class WrongCat : virtual public WrongAnimal {
+  public:
+	WrongCat();
+	WrongCat(const std::string &name);
+	WrongCat(const WrongCat &w);
+	const WrongCat &operator=(const WrongCat &w);
+	~WrongCat();
+
+  public:
+	// Following the instructions, not camelcase.
+	void makeSound() const;
+};
+
+#endif /* EX00_WRONG_CAT_HPP */
