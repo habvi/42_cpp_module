1c9380bb68bec424bdb55db18aecb81e0aa7aa57
habvi <93956895+habvi@users.noreply.github.com>
2024-02-28 23:57:50 +0900

    add(Dog,Cat): catch allocate error & exit

--- 04/ex01/srcs/Dog.cpp
+++ 04/ex01/srcs/Dog.cpp
@@ -1,6 +1,7 @@
 #include "Dog.hpp"
 #include "Brain.hpp"
 #include "color.hpp"
+#include <cstdlib>
 #include <iostream>
 #include <string>
 
@@ -9,36 +10,56 @@
 
 Dog::Dog() : Animal() {
 	std::cerr << "Default constructor called (" TYPE_DOG ")" << std::endl;
+	try {
+		brain_ = new Brain;
+	} catch (const std::bad_alloc &e) {
+		std::cerr << COLOR_RED "Error: failed to allocate: " << e.what() << COLOR_END
+				  << std::endl;
+		std::exit(EXIT_FAILURE);
+	}
 	SetType(TYPE_DOG);
-	// todo: for new fail, throw exception
-	brain_ = new Brain;
 }
 
 Dog::Dog(const std::string &name) : Animal() {
 	std::cerr << "Constructor called (" TYPE_DOG ")" << std::endl;
+	try {
+		brain_ = new Brain;
+	} catch (const std::bad_alloc &e) {
+		std::cerr << COLOR_RED "Error: failed to allocate: " << e.what() << COLOR_END
+				  << std::endl;
+		std::exit(EXIT_FAILURE);
+	}
 	SetType(name);
-	// todo: for new fail, throw exception
-	brain_ = new Brain;
 }
 
 void Dog::CopyBrain(const Dog &d) {
-	// todo: for new fail, throw exception
 	brain_ = new Brain(d.GetBrain());
 }
 
 Dog::Dog(const Dog &d) : Animal(d) {
 	std::cerr << "Coppy constructor called (" TYPE_DOG ")" << std::endl;
+	try {
+		CopyBrain(d);
+	} catch (const std::bad_alloc &e) {
+		std::cerr << COLOR_RED "Error: failed to allocate: " << e.what() << COLOR_END
+				  << std::endl;
+		std::exit(EXIT_FAILURE);
+	}
 	SetType(d.getType());
-	CopyBrain(d);
 }
 
 const Dog &Dog::operator=(const Dog &d) {
 	std::cerr << "Copy assignment operator= called (" TYPE_DOG ")" << std::endl;
 	if (this != &d) {
-		SetType(d.getType());
-
 		delete brain_;
-		CopyBrain(d);
+		try {
+			CopyBrain(d);
+		} catch (const std::bad_alloc &e) {
+			std::cerr << COLOR_RED "Error: failed to allocate: " << e.what()
+					  << COLOR_END << std::endl;
+			std::exit(EXIT_FAILURE);
+		}
+		SetType(d.getType());
 	}
 	return *this;
 }
