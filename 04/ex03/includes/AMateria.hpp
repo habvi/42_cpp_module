c91af71b28c33f33021a8790c88a0871f8269bd7
habvi <93956895+habvi@users.noreply.github.com>
2024-03-03 00:30:03 +0900

    add(AMateria): implement AMateria class

---
+++ 04/ex03/includes/AMateria.hpp
@@ -0,0 +1,28 @@
+#ifndef EX03_AMateria_HPP
+#define EX03_AMateria_HPP
+
+#include "ICharacter.hpp"
+#include <string>
+
+class AMateria {
+  public:
+	virtual ~AMateria();
+
+  public:
+	virtual AMateria *clone() const = 0;
+	virtual void      use(ICharacter &target);
+
+  public:
+	std::string const &getType() const;
+
+  protected:
+	AMateria();
+	explicit AMateria(std::string const &type);
+	AMateria(const AMateria &a);
+	const AMateria &operator=(const AMateria &a);
+
+  private:
+	std::string type_;
+};
+
+#endif /* EX03_AMateria_HPP */
