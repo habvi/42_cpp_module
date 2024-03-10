a86528a70b74e86d00324a2942c223bd6e600673
habvi <93956895+habvi@users.noreply.github.com>
2024-03-11 06:42:10 +0900

    add(Intern): implement Intern class base

---
+++ 05/ex03/srcs/Intern.cpp
@@ -0,0 +1,55 @@
+#include "Intern.hpp"
+#include "PresidentialPardonForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "ShrubberyCreationForm.hpp"
+#include "color.hpp"
+#include <iostream>
+#include <new>
+
+Intern::Intern() {}
+
+Intern::Intern(const Intern &i) {
+	(void)i;
+}
+
+const Intern &Intern::operator=(const Intern &i) {
+	(void)i;
+	return *this;
+}
+
+Intern::~Intern() {}
+
+AForm *TryMakeForm(const std::string &form_name, const std::string &target_form) {
+	AForm *form = NULL;
+
+	// todo: don't use if/else
+	if (form_name == "presidential pardon") {
+		form = new (std::nothrow) PresidentialPardonForm(target_form);
+	} else if (form_name == "robotomy request") {
+		form = new (std::nothrow) RobotomyRequestForm(target_form);
+	} else if (form_name == "shrubbery creation") {
+		form = new (std::nothrow) ShrubberyCreationForm(target_form);
+	} else {
+		throw std::logic_error("Error: invalid Form name");
+	}
+	if (form == NULL) {
+		throw;
+	}
+	return form;
+}
+
+AForm *
+Intern::makeForm(const std::string &form_name, const std::string &target_form) {
+	AForm *form;
+
+	try {
+		form = TryMakeForm(form_name, target_form);
+		std::cout << COLOR_PINK "Intern creats " << form_name << COLOR_END
+				  << std::endl;
+	} catch (const std::exception &e) {
+		std::cerr << COLOR_PINK << "Intern creats Form failed. => " COLOR_RED
+				  << e.what() << COLOR_END << std::endl;
+		return NULL;
+	}
+	return form;
+}
