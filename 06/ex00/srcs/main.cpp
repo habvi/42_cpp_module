1dfa130bc29fb0a9f31f8eb5cbea828e4f5f47f0
habvi <93956895+habvi@users.noreply.github.com>
2024-04-20 20:01:25 +0900

    add(main): main() base

--- 06/ex00/srcs/main.cpp
+++ 06/ex00/srcs/main.cpp
@@ -1,4 +1,22 @@
 #include "ScalarConverter.hpp"
+#include <cstdlib>
 #include <iostream>
 
-int main() {}
+static void RunOriginalTest() {}
+
+static void RunLikeSubject(const std::string &str) {
+	ScalarConverter::convert(str);
+}
+
+int main(int argc, const char **argv) {
+	if (argc == 1) {
+		RunOriginalTest();
+	} else if (argc == 2) {
+		RunLikeSubject(std::string(argv[1]));
+	} else {
+		std::cerr << "Usage:\n"
+				  << "  [subject] ./convert <argument>\n"
+				  << "  [test]    ./convert" << std::endl;
+	}
+	return EXIT_SUCCESS;
+}
