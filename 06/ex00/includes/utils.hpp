b470d6950703c43c90cd5ab7b9ef983520f93b03
habvi <93956895+habvi@users.noreply.github.com>
2024-04-21 01:57:30 +0900

    fix(utils): add include guard

--- 06/ex00/includes/utils.hpp
+++ 06/ex00/includes/utils.hpp
@@ -1,3 +1,6 @@
+#ifndef EX00_UTILS_HPP
+#define EX00_UTILS_HPP
+
 #include <limits>
 
 template <typename T, typename U>
@@ -20,3 +23,5 @@ template <typename T>
 bool IsInfinityOrNan(const T &num) {
 	return IsInfinity(num) || IsNan(num);
 }
+
+#endif /* EX00_UTILS_HPP */
