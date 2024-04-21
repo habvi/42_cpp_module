b3a8c61bbb53f939c729a1095a87544e4745bc4a
habvi <93956895+habvi@users.noreply.github.com>
2024-04-22 03:30:24 +0900

    add(main): identify each type by Base*

--- 06/ex02/srcs/main.cpp
+++ 06/ex02/srcs/main.cpp
@@ -6,6 +6,8 @@
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
+#include <stdexcept>
+#include <string>
 
 Base *generate(void) {
 	const int random_num = std::rand() % 3;
@@ -26,10 +28,39 @@ Base *generate(void) {
 	return base;
 }
 
+void print(const std::string &type) {
+	std::cout << "Actual type is: " COLOR_GREEN << type << COLOR_END << std::endl;
+}
+
+void identify(Base *p) {
+	if (p == NULL) {
+		throw std::invalid_argument("argument is NULL");
+	}
+	if (dynamic_cast<A *>(p)) {
+		print("A");
+	} else if (dynamic_cast<B *>(p)) {
+		print("B");
+	} else if (dynamic_cast<C *>(p)) {
+		print("C");
+	} else {
+		throw std::invalid_argument("invalid argument type");
+	}
+}
+
 int main() {
 	std::srand(std::time(0));
 
 	Base *p = generate();
+	identify(p);
 	delete p;
+
+	std::cout << "------------------" << std::endl;
+	// try passed NULL
+	try {
+		identify(NULL);
+	} catch (const std::exception &e) {
+		std::cerr << COLOR_RED << e.what() << COLOR_END << std::endl;
+	}
+
 	return EXIT_SUCCESS;
 }
