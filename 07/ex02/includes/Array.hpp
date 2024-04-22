23540abeece7acf6eb3329b068cd4e2202a71b77
habvi <93956895+habvi@users.noreply.github.com>
2024-04-22 22:26:14 +0900

    update(Array): move class's function template implementation to .tpp file

--- 07/ex02/includes/Array.hpp
+++ 07/ex02/includes/Array.hpp
@@ -1,62 +1,31 @@
 #ifndef EX02_ARRAY_HPP
 #define EX02_ARRAY_HPP
 
-#include <stdexcept>
-
 template <typename T>
 class Array {
   public:
-	Array() : array_(NULL){};
-	Array(const unsigned int n) : array_(new T[n]), size_(n){};
-	Array(const Array &other) {
-		*this = Deepcopy(other);
-	};
-	~Array() {
-		delete[] array_;
-	};
-	const Array &operator=(const Array &other) {
-		if (this != &other) {
-			delete[] array_;
-			*this = Deepcopy(other);
-		}
-		return *this;
-	};
+	Array();
+	Array(const unsigned int n);
+	Array(const Array &other);
+	~Array();
+	const Array &operator=(const Array &other);
 	// for read : [i]
-	const T &operator[](const unsigned int index) const {
-		ThrowIfIndexOutOfRange(index);
-		return array_[index];
-	};
+	const T &operator[](const unsigned int index) const;
 	// for write : [i] = x
-	T &operator[](const unsigned int index) {
-		ThrowIfIndexOutOfRange(index);
-		return array_[index];
-	};
+	T &operator[](const unsigned int index);
 
   public:
-	unsigned int size() const {
-		return size_;
-	};
+	unsigned int size() const;
 
   private:
-	Array &Deepcopy(const Array &other) {
-		size_  = other.size();
-		array_ = new T[size_];
-		for (unsigned int i = 0; i < size_; i++) {
-			array_[i] = other[i];
-		}
-		return *this;
-	}
-	void ThrowIfIndexOutOfRange(const unsigned int index) const {
-		if (index >= size_) {
-			throw std::out_of_range("index out of bounds");
-		}
-	}
+	Array &Deepcopy(const Array &other);
+	void   ThrowIfIndexOutOfRange(const unsigned int index) const;
 
   private:
 	T           *array_;
 	unsigned int size_;
 };
 
-// #include "Array.tpp"
+#include "Array.tpp"
 
 #endif /* EX02_ARRAY_HPP */
---
+++ 07/ex02/includes/Array.tpp
@@ -0,0 +1,62 @@
+#include <stdexcept>
+
+template <typename T>
+Array<T>::Array() : array_(NULL), size_(0) {}
+
+template <typename T>
+Array<T>::Array(const unsigned int n) : array_(new T[n]), size_(n) {}
+
+template <typename T>
+Array<T>::Array(const Array &other) {
+	Deepcopy(other);
+}
+
+template <typename T>
+Array<T>::~Array() {
+	delete[] array_;
+}
+
+template <typename T>
+const Array<T> &Array<T>::operator=(const Array &other) {
+	if (this != &other) {
+		delete[] array_;
+		Deepcopy(other);
+	}
+	return *this;
+}
+
+template <typename T>
+const T &Array<T>::operator[](const unsigned int index) const {
+	ThrowIfIndexOutOfRange(index);
+	return array_[index];
+}
+
+template <typename T>
+T &Array<T>::operator[](const unsigned int index) {
+	ThrowIfIndexOutOfRange(index);
+	return array_[index];
+}
+
+// --------------------------------------------------------------------
+template <typename T>
+unsigned int Array<T>::size() const {
+	return size_;
+}
+
+// --------------------------------------------------------------------
+template <typename T>
+Array<T> &Array<T>::Deepcopy(const Array &other) {
+	size_  = other.size();
+	array_ = new T[size_];
+	for (unsigned int i = 0; i < size_; i++) {
+		array_[i] = other[i];
+	}
+	return *this;
+}
+
+template <typename T>
+void Array<T>::ThrowIfIndexOutOfRange(const unsigned int index) const {
+	if (index >= size_) {
+		throw std::out_of_range("index out of bounds");
+	}
+}
