743477cdb98574849963f9df015a9d7337b774b6
habvi <93956895+habvi@users.noreply.github.com>
2024-04-23 00:51:26 +0900

    add(main): test for stringArray, emptyArray, 2DArray

--- 07/ex02/srcs/main.cpp
+++ 07/ex02/srcs/main.cpp
@@ -5,6 +5,13 @@
 #include <iostream>
 #include <string>
 
+static void
+DisplayTitle(const unsigned int testcase_number, const std::string &title) {
+	std::cout << "\n┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" << std::endl;
+	std::cout << "┃ test " << testcase_number << ": " << title << std::endl;
+	std::cout << "┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" << std::endl;
+}
+
 static void Line() {
 	std::cout << "-----------------------------" << std::endl;
 }
@@ -73,6 +80,8 @@ JudgeIsSameArray(const Array<T> &a, const T *b, const unsigned int b_size) {
 
 // --------------------------------------------------------------------
 static void RunSubjectTest() {
+	DisplayTitle(1, "int Array");
+
 	const unsigned int kArraySize = 5;
 
 	Array<int> numbers(kArraySize);
@@ -113,9 +122,139 @@ static void RunSubjectTest() {
 	} catch (const std::exception &e) {
 		std::cerr << COLOR_RED << e.what() << COLOR_END << std::endl;
 	}
-	Line();
 
 	delete[] mirror;
+}
+
+// --------------------------------------------------------------------
+static void RunOriginalTest1() {
+	DisplayTitle(2, "string Array");
+
+	const unsigned int kArraySize = 7;
+
+	Array<std::string> strs(kArraySize);
+	std::string       *mirror = new std::string[kArraySize];
+	for (unsigned int i = 0; i < kArraySize; i++) {
+		const std::string &s = "a";
+		strs[i]              = s;
+		strs[i].push_back('a' + i);
+		mirror[i] = s;
+		mirror[i].push_back('a' + i);
+	}
+	PrintArray("strs  ", strs);
+	PrintArray("mirror", mirror, kArraySize);
+	JudgeIsSameArray(strs, mirror, kArraySize);
+	delete[] mirror;
+
+	// copy
+	Array<std::string> tmp1 = strs;
+	PrintArray("strs", strs);
+	PrintArray("tmp1", tmp1);
+	JudgeIsSameArray(strs, tmp1);
+
+	Array<std::string> tmp2(tmp1);
+	PrintArray("tmp1", tmp1);
+	PrintArray("tmp2", tmp2);
+	JudgeIsSameArray(tmp1, tmp2);
+
+	// error
+	try {
+		strs[1000] = "errrrr";
+	} catch (const std::exception &e) {
+		std::cerr << COLOR_RED << e.what() << COLOR_END << std::endl;
+	}
+}
+
+static void RunOriginalTest2() {
+	DisplayTitle(3, "empty Array");
+
+	const unsigned int kArraySize = 0;
+
+	Array<double> arr;
+	double       *mirror = new double[kArraySize];
+	PrintArray("arr   ", arr);
+	PrintArray("mirror", mirror, kArraySize);
+	JudgeIsSameArray(arr, mirror, kArraySize);
+	delete[] mirror;
+
+	// copy
+	Array<double> tmp1 = arr;
+	PrintArray("arr ", arr);
+	PrintArray("tmp1", tmp1);
+	JudgeIsSameArray(arr, tmp1);
+
+	Array<double> tmp2(tmp1);
+	PrintArray("tmp1", tmp1);
+	PrintArray("tmp2", tmp2);
+	JudgeIsSameArray(tmp1, tmp2);
+
+	// error
+	for (int i = -2; i < 2; i++) {
+		try {
+			arr[i] = 123.456;
+		} catch (const std::exception &e) {
+			std::cerr << COLOR_RED << i << ": " << e.what() << COLOR_END
+					  << std::endl;
+		}
+	}
+}
+
+// --------------------------------------------------------------------
+template <typename T>
+static void
+JudgeIsSame2DArray(const Array<Array<T> > &a, const Array<Array<T> > &b) {
+	if (a.size() != b.size()) {
+		std::cerr << COLOR_RED "Error: Not the same array" COLOR_END << std::endl;
+		exit(EXIT_FAILURE);
+	}
+	for (unsigned int i = 0; i < a.size(); i++) {
+		std::cout << "raw " << i << ": ";
+		JudgeIsSameArray(a[i], b[i]);
+	}
+}
+
+static void
+Print2DArray(const std::string &title, const Array<Array<int> > &array2D) {
+	std::cout << title << " = {" << std::endl;
+	for (unsigned int i = 0; i < array2D.size(); ++i) {
+		std::cout << "   ";
+		for (unsigned int j = 0; j < array2D[i].size(); ++j) {
+			std::cout << array2D[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
+	std::cout << "}" << std::endl;
+}
+
+static void RunOriginalTest3() {
+	DisplayTitle(4, "2D Array");
+
+	const unsigned int kRowSize = 3;
+	const unsigned int kColSize = 5;
+
+	Array<Array<int> > array2D(kRowSize);
+	for (unsigned int i = 0; i < kRowSize; i++) {
+		array2D[i] = Array<int>(kColSize);
+		for (unsigned int j = 0; j < kColSize; ++j) {
+			array2D[i][j] = rand() % 100;
+		}
+	}
+	Print2DArray("2D array", array2D);
+	Line();
+
+	// copy
+	Array<Array<int> > tmp(array2D);
+	Print2DArray("tmp", tmp);
+	Line();
+
+	JudgeIsSame2DArray(array2D, tmp);
+
+	// error
+	try {
+		array2D[2][kColSize] = 5;
+	} catch (const std::exception &e) {
+		std::cerr << COLOR_RED << e.what() << COLOR_END << std::endl;
+	}
 }
 
 // --------------------------------------------------------------------
@@ -123,7 +262,9 @@ int main() {
 	srand(time(NULL));
 
 	RunSubjectTest();
-	// Line();
-	// RunOriginalTest();
+	RunOriginalTest1();
+	RunOriginalTest2();
+	RunOriginalTest3();
+
 	return EXIT_SUCCESS;
 }
