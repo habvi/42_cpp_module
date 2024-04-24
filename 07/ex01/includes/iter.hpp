a123da7f1c655c5dff247003a8c39ad2309ef11d
habvi <93956895+habvi@users.noreply.github.com>
2024-04-24 15:12:04 +0900

    update(iter): for const T *array (#48)

--- 07/ex01/includes/iter.hpp
+++ 07/ex01/includes/iter.hpp
@@ -4,7 +4,17 @@
 #include <cstddef>
 
 template <typename T>
-void iter(T *array, const size_t length, void (*func)(T &)) {
+void iter(T *array, const size_t &length, void (*func)(T &)) {
+	if (!array || !func) {
+		return;
+	}
+	for (size_t i = 0; i < length; i++) {
+		func(array[i]);
+	}
+}
+
+template <typename T>
+void iter(const T *array, const size_t &length, void (*func)(T &)) {
 	if (!array || !func) {
 		return;
 	}
