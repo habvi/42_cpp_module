e90bb59c108de85d2ae72e4c6855b2d6c0f49eeb
habvi <93956895+habvi@users.noreply.github.com>
2024-04-26 19:18:49 +0900

    update(MutantStack): add reverse iterators rbegin/rend (#53)

--- 08/ex02/includes/MutantStack.hpp
+++ 08/ex02/includes/MutantStack.hpp
@@ -7,9 +7,11 @@
 template <typename T>
 class MutantStack : public std::stack<T> {
   public:
-	typedef typename std::stack<T>::container_type Container;
-	typedef typename Container::iterator           iterator;
-	typedef typename Container::const_iterator     const_iterator;
+	typedef typename std::stack<T>::container_type     Container;
+	typedef typename Container::iterator               iterator;
+	typedef typename Container::const_iterator         const_iterator;
+	typedef typename Container::reverse_iterator       reverse_iterator;
+	typedef typename Container::const_reverse_iterator const_reverse_iterator;
 
   public:
 	MutantStack();
@@ -18,10 +20,14 @@ class MutantStack : public std::stack<T> {
 	~MutantStack();
 
   public:
-	iterator       begin();
-	const_iterator begin() const;
-	iterator       end();
-	const_iterator end() const;
+	iterator               begin();
+	const_iterator         begin() const;
+	iterator               end();
+	const_iterator         end() const;
+	reverse_iterator       rbegin();
+	const_reverse_iterator rbegin() const;
+	reverse_iterator       rend();
+	const_reverse_iterator rend() const;
 
   public:
 	void DebugPrint();
